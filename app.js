b7f0a4465b36f45b53b7d4ac7fe50350b7f57c5c
david <tyson.b@hotmail.fr>
2024-01-19 16:24:56 +0100

    Add README

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log(3+4);
+
+console.log(Math.min(2,3,1));
+
+console.log("Bienvenue" + " dans" + " le cours de Javascript");
+
+console.log("3 + 4 = " + (3 + 4)
++ ", et le minimum de (6,3,4) vaut "
++ Math.min(6,3,4));
+
+
+window.alert("- Myriam : " + Math.min(20 - 8) + "/20");
+window.alert("- Anna : " + Math.min(20 - 0) + "/20");
+window.alert("- Charles : " + Math.min(20 - 4) + "/20.");
+
+
+// Définir les valeurs
+var valeur1 = 12;
+var valeur2 = 20;
+var valeur3 = 16;
+
+// Calculer la moyenne
+var moyenne = (valeur1 + valeur2 + valeur3) / 3;
+
+// Afficher la moyenne
+window.alert("La moyenne est " + moyenne + "/20")
